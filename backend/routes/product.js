1406aa6810a2c3aacdc658d8f6fab1f82c5b242f
kurevijaa <kurevijaa@gmail.com>
2021-07-31 18:48:32 +0200

    reviews create,update,display

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,7 +6,9 @@ const { getProducts,
     newProduct, 
     getSingleProduct, 
     updateProduct, 
-    deleteProduct 
+    deleteProduct,
+    createProductReview,
+    getProductReviews
 } = require('../controllers/productController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
@@ -23,6 +25,11 @@ router.route('/admin/product/new').post(isAuthenticatedUser,authorizeRoles('admi
 router.route('/admin/product/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct);
 
 router.route('/admin/product/:id').delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct);
+
+
+router.route('/review').put(isAuthenticatedUser, createProductReview);
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews);
+
 
 
 module.exports=router;
