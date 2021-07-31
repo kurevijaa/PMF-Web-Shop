b16bc416b07935321ffe0a9d4814723159931d45
kurevijaa <kurevijaa@gmail.com>
2021-07-31 17:16:02 +0200

    order update/process/delete/

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,6 +3,7 @@ const Product = require('../models/product');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const order = require('../models/order');
 //const order = require('../models/order');
 
 
@@ -61,7 +62,7 @@ exports.myOrders = catchAsyncErrors(async(req,res,next) => {
     })
 })
 
-// Dohvati sve narudžbe  => /api/v1/admin/orders
+// Dohvati sve narudžbe - ADMIN  => /api/v1/admin/orders
 exports.allOrders = catchAsyncErrors(async(req,res,next) => {
     const orders = await Order.find()
 
@@ -75,5 +76,49 @@ exports.allOrders = catchAsyncErrors(async(req,res,next) => {
         success: true,
         totalAmount,
         orders
+    })
+})
+
+// Ažuriranje/obrada narudžbe - ADMIN  => /api/v1/admin/orders
+exports.updateOrders = catchAsyncErrors(async(req,res,next) => {
+    const order = await Order.findById(req.params.id)
+
+    if(order.orderStatus === 'Delivered'){
+        return next(new ErrorHandler('Narudžba je već dostavljena', 400))
+    }
+
+    order.orderItems.forEach(async item => {
+        await updateStock(item.product, item.quantity)
+    })
+
+    order.orderStatus = req.body.status,
+        order.deliveredAt = Date.now()
+
+    await order.save()
+
+    res.status(200).json({
+        success: true,
+    })
+})
+
+async function updateStock(id,quantity){
+    const product = await Product.findById(id);
+    product.stock = product.stock - quantity;
+
+    await product.save({validateBeforeSave: false})
+}
+
+// Izbriši narudžbu => /api/v1/admin/order/:id
+exports.deleteOrder = catchAsyncErrors(async(req,res,next) => {
+    const order = await Order.findById(req.params.id)
+
+    if(!order) {
+        return next(new ErrorHandler('Narudžba nije pronađena', 404))
+    }
+
+    await order.remove()
+
+    res.status(200).json({
+        success: true
     })
 })
