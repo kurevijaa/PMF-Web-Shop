a2f4ce250fdf3bbfb1a5fb20bd1c12da0aee63bb
kurevijaa <kurevijaa@gmail.com>
2021-09-04 13:12:42 +0200

    url error solved with protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Login from './components/user/Login'
 import Register from './components/user/Register'
 import Profile from './components/user/Profile'
 
+import ProtectedRoute from './components/route/ProtectedRoute';
 import { loadUser } from './actions/userActions'
 import store from './store'
 
@@ -30,7 +31,7 @@ function App() {
       <Route path="/Trgovina" component={StorePage} exact />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
-      <Route path="/me" component={Profile} exact/>
+      <ProtectedRoute path="/me" component={Profile} exact/>
 
 
 
